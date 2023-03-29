cdc8b08f33aa8c567a936cd7819657314a244207
Tony Wiedman <107732340+tonywied17@users.noreply.github.com>
2023-03-29 19:48:58 -0400

    env

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
+
 module.exports = {
-  HOST: "",
-  USER: "",
-  PASSWORD: "",
-  DB: "pennsylvania",
+  HOST: process.env.DB_HOST,
+  USER: process.env.DB_USER,
+  PASSWORD: process.env.DB_PASSWORD,
+  DB: process.env.DB_NAME,
   dialect: "mysql",
   pool: {
     max: 5,
