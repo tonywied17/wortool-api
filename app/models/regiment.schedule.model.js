02f0de2b8d7e739ee4bd499e4908dd36b002b61c
Tony Wiedman <107732340+tonywied17@users.noreply.github.com>
2023-08-01 11:46:08 -0400

    put/update current controllers

--- app/models/regiment.schedule.model.js
+++ app/models/regiment.schedule.model.js
@@ -4,7 +4,7 @@
  * Created Date: Tuesday August 1st 2023
  * Author: Tony Wiedman
  * -----
- * Last Modified: Tue August 1st 2023 11:23:16 
+ * Last Modified: Tue August 1st 2023 11:45:42 
  * Modified By: Tony Wiedman
  * -----
  * Copyright (c) 2023 Tone Web Design, Molex
@@ -60,3 +60,35 @@ module.exports = (sequelize, Sequelize) => {
 
     return RegSchedule;
 };
+
+/**
+ * Update Current Regiment Schedule
+ * This function updates the current Regiment Schedule.
+ * @param {*} req - Request containing the updated Regiment Schedule data in the body
+ * @param {*} res - Response containing the updated Regiment Schedule data
+ */
+exports.updateSchedule = (req, res) => {
+    const id = req.params.id;
+
+    RegSchedule.update(req.body, {
+            where: {
+                id: id,
+            },
+        })
+        .then((num) => {
+            if (num == 1) {
+                res.send({
+                    message: "Schedule was updated successfully.",
+                });
+            } else {
+                res.send({
+                    message: `Cannot update Schedule with id=${id}. Maybe Schedule was not found or req.body is empty!`,
+                });
+            }
+        })
+        .catch((err) => {
+            res.status(500).send({
+                message: "Error updating Schedule with id=" + id,
+            });
+        });
+}
